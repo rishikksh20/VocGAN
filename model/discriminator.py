900f5e4a1b1cc6485b5d4c66166ee1169af65281
Rishikesh <rishikksh20@gmail.com>
2020-08-06 02:33:52 +0530

    Add JCU discriminator

--- model/discriminator.py
+++ model/discriminator.py
@@ -52,7 +52,33 @@ class Discriminator(nn.Module):
             features.append(x)
         return features[:-1], features[-1]
 
+class JCU_Discriminator(nn.Module):
+  def __init__(self):
+    super(JCU_Discriminator, self).__init__()
+    mel_conv1 = nn.Conv1d(80, 128, kernel_size=2, stride=1)
+    mel_conv2 = nn.Conv1d(128, 128, kernel_size=5, stride=1, padding=2)
+    mel_conv3 = nn.Conv1d(128, 1, kernel_size=3, stride=1, padding=1)
 
+    x_conv1 = nn.Conv1d(1, 16, kernel_size=15, stride=1)
+    x_conv2 = nn.Conv1d(16, 64, kernel_size=41, stride=4, padding=4 * 5,
+                groups=16 // 4)
+    x_conv3 = nn.Conv1d(64, 128, kernel_size=21, stride=2)
+    x_conv4 = nn.Conv1d(128, 128, kernel_size=5, stride=1, padding=2)
+    x_conv5 = nn.Conv1d(128, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x, mel):
+      out = mel_conv1(mel)
+      out1 = x_conv1(x)
+      out1 = x_conv2(out1)
+      out1 = x_conv3(out1)
+      out = torch.cat([out, out1], dim=2)
+      out = mel_conv2(out)
+      cond_out = mel_conv3(out)
+      out1 = x_conv4(out1)
+      uncond_out = x_conv5(out1)
+      return cond_out, uncond_out
+
+    
 if __name__ == '__main__':
     model = Discriminator()
     '''
